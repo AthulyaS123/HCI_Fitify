bcb041f7e46bd1c04d811a9816fb4411954beadf
mansimadhani <madhanimansi56@gmail.com>
2025-04-14 10:16:44 -0500

    added new clothes

--- my-app/src/Screens/Clothes/index.js
+++ my-app/src/Screens/Clothes/index.js
@@ -74,3 +74,85 @@ export { default as unishoe4 } from './uni-shoe-4.png';
 export { default as unishoe5 } from './uni-shoe-5.png';
 export { default as unishoe6 } from './uni-shoe-6.png'; 
 
+// ITEMS 55-80
+export { default as item55 } from './item55.png';
+export { default as item56 } from './item56.png';
+export { default as item57 } from './item57.png';
+export { default as item58 } from './item58.png';
+export { default as item59 } from './item59.png';
+export { default as item60 } from './item60.png';
+export { default as item61 } from './item61.png';
+export { default as item62 } from './item62.png';
+export { default as item63 } from './item63.png';
+export { default as item64 } from './item64.png';
+export { default as item65 } from './item65.png';
+export { default as item66 } from './item66.png';
+export { default as item67 } from './item67.png';
+export { default as item68 } from './item68.png';
+export { default as item69 } from './item69.png';
+export { default as item70 } from './item70.png';
+export { default as item71 } from './item71.png';
+export { default as item72 } from './item72.png';
+export { default as item73 } from './item73.png';
+export { default as item74 } from './item74.png';
+export { default as item75 } from './item75.png';
+export { default as item76 } from './item76.png';
+export { default as item77 } from './item77.png';
+export { default as item78 } from './item78.png';
+export { default as item79 } from './item79.png';
+export { default as item80 } from './item80.png';
+
+// ITEMS 81-106
+export { default as item81 } from './item81.png';
+export { default as item82 } from './item82.png';
+export { default as item83 } from './item83.png';
+export { default as item84 } from './item84.png';
+export { default as item85 } from './item85.png';
+export { default as item86 } from './item86.png';
+export { default as item87 } from './item87.png';
+export { default as item88 } from './item88.png';
+export { default as item89 } from './item89.png';
+export { default as item90 } from './item90.png';
+export { default as item91 } from './item91.png';
+export { default as item92 } from './item92.png';
+export { default as item93 } from './item93.png';
+export { default as item94 } from './item94.png';
+export { default as item95 } from './item95.png';
+export { default as item96 } from './item96.png';
+export { default as item97 } from './item97.png';
+export { default as item98 } from './item98.png';
+export { default as item99 } from './item99.png';
+export { default as item100 } from './item100.png';
+export { default as item101 } from './item101.png';
+export { default as item102 } from './item102.png';
+export { default as item103 } from './item103.png';
+export { default as item104 } from './item104.png';
+export { default as item105 } from './item105.png';
+export { default as item106 } from './item106.png';
+
+// ITEMS 107-
+export { default as item107 } from './item107.png';
+export { default as item108 } from './item108.png';
+export { default as item109 } from './item109.png';
+export { default as item110 } from './item110.png';
+export { default as item111 } from './item111.png';
+export { default as item112 } from './item112.png';
+export { default as item113 } from './item113.png';
+export { default as item114 } from './item114.png';
+export { default as item115 } from './item115.png';
+export { default as item116 } from './item116.png';
+export { default as item117 } from './item117.png';
+export { default as item118 } from './item118.png';
+export { default as item119 } from './item119.png';
+export { default as item120 } from './item120.png';
+export { default as item121 } from './item121.png';
+export { default as item122 } from './item122.png';
+export { default as item123 } from './item123.png';
+export { default as item124 } from './item124.png';
+
+
+
+
+
+
+
